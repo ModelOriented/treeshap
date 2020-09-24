22bcba1791ad3e8e6ccda30d3004ccf78c0af2a5
konrad-komisarczyk <komisarczykkonrad@gmail.com>
2020-09-24 15:12:52 +0200

    first version of full break-down implementation

--- R/plot.R
+++ R/plot.R
@@ -50,12 +50,12 @@ plot_feature_importance <- function(shaps,
   df <- df[order(df$importance, decreasing = TRUE)[1:max_vars], ]
 
   # plot it
-  pl <- ggplot(df, aes(x = variable, y = importance)) +
+  p <- ggplot(df, aes(x = variable, y = importance)) +
     geom_bar(stat = "identity", fill = DALEX::colors_discrete_drwhy(1))
 
   # TODO: (?) boxplot
 
-  pl + coord_flip() +
+  p + coord_flip() +
     DALEX::theme_drwhy_vertical() +
     ylab("mean(|SHAP value|)") + xlab("") +
     labs(title = title, subtitle = subtitle) +
@@ -63,156 +63,6 @@ plot_feature_importance <- function(shaps,
     theme(legend.position = "none")
 }
 
-#' SHAP value based Break-Down plot
-#'b
-#' This function plots contributions of features into the prediction of a single observation.
-#'
-#' @param shap SHAP values dataframe produced with the \code{treeshap} function, containing only one row.
-#' @param x \code{NULL} or dataframe with 1 observation used to calculate \code{shap}.
-#' Used only for aesthetic reasons - to include observation values for a different variables next to the variable names in labels on the y axis.
-#' By default is \code{NULL} and then labels on the y axis are just variable names.
-#' @param model \code{NULL} or dataframe containing unified representation of explained model created with a (model).unify function.
-#' @param max_vars maximum number of variables that shall be presented. Variable with highest importance will be presented.
-#' Other variables will be summed into one contribution. By default \code{5}.
-#' @param title the plot's title, by default \code{'SHAP based Break-Down profile'}.
-#' @param subtitle the plot's subtitle. By default no subtitle.
-#'
-#' @return a \code{ggplot2} object
-#'
-#' @export
-#'
-#' @seealso
-#' \code{\link{treeshap}} for calculation of SHAP values
-#'
-#' @examples
-#' \dontrun{
-#' library(xgboost)
-#' data <- fifa20$data[colnames(fifa20$data) != 'work_rate']
-#' target <- fifa20$target
-#' param <- list(objective = "reg:squarederror", max_depth = 3)
-#' xgb_model <- xgboost::xgboost(as.matrix(data), params = param, label = target, nrounds = 200)
-#' unified_model <- xgboost.unify(xgb_model)
-#' shaps <- treeshap(unified_model, head(data, 3))
-#' plot_feature_importance(shaps, max_vars = 4)
-#' }
-plot_contribution <- function(shap,
-                              x = NULL,
-                              model = NULL,
-                              max_vars = 5,
-                              title = "SHAP based Break-Down profile",
-                              subtitle = "") {
-  #position <- cumulative <- prev <- pretty_text <- right_side <- contribution <- NULL
-
-  if (max_vars > ncol(shap)) {
-    warning("max_vars exceeds number of variables. All variables will be shown.")
-    max_vars <- ncol(shap)
-  }
-
-  if (nrow(shap) != 1) {
-    warning("Only 1 observation can be plotted. Plotting 1st one.")
-    shap <- shap[1, ]
-  }
-
-  # calculating model's mean prediction
-  if (!is.null(model)) {
-    if (!all(c("Tree", "Node", "Feature", "Split", "Yes", "No", "Missing", "Quality/Score", "Cover") %in% colnames(model))) {
-      stop("Given model dataframe is not a correct unified dataframe representation. Use (model).unify function.")
-    }
-    is_leaf <- is.na(model$Feature)
-    is_root <- model$Node == 0
-    mean_prediction <- sum(model[is_leaf, "Quality/Score"] * model$Cover[is_leaf]) / sum(model$Cover[is_root])
-  }
-
-
-  df <- data.frame(variable = colnames(shap), contribution = as.numeric(shap))
-
-  # if using observation values, then setting variable names to showing their value
-  if (!is.null(x)) {
-    if (!all(colnames(x) == colnames(shap))) {
-      stop("shap and x should have the same variables.")
-    }
-    if (nrow(x) != 1) {
-      warning("Only 1 observation can be plotted. Assuming first observations in x and in shap are the same.")
-      x <- x[1, ]
-    }
-    df$variable <- paste0(df$variable, " = ", as.character(x))
-  }
-
-  # selecting max_vars most important variables
-  is_important <- order(abs(df$contribution), decreasing = TRUE)[1:max_vars]
-  other_variables_contribution_sum <- sum(df$contribution[-is_important])
-  df <- df[is_important, ]
-  if (max_vars < ncol(shap)) {
-    df <- rbind(df, data.frame(variable = "+ all other variables", contribution = other_variables_contribution_sum))
-  }
-
-
-
-  # # enrich dataframe with additional features
-  # tmp <- prepare_data_for_break_down_plot(x, baseline, rounding_function, digits)
-  # broken_baseline <- tmp$broken_baseline
-  # x <- tmp$x
-  #
-  # # fix for https://github.com/ModelOriented/iBreakDown/issues/85
-  # # check if correction is needed
-  # if (any(x[x$variable == "prediction", "right_side"] < broken_baseline$contribution)) {
-  #   # put there max val
-  #   x[x$variable == "prediction", "right_side"] <- pmax(x[x$variable == "prediction", "right_side"], broken_baseline$contribution)
-  # }
-  # if (any(x[x$variable == "intercept", "right_side"] < broken_baseline$contribution)) {
-  #   # put there max val
-  #   x[x$variable == "intercept", "right_side"] <- pmax(x[x$variable == "intercept", "right_side"], broken_baseline$contribution)
-  # }
-  #
-  #
-  # # base plot
-  # pl <- ggplot(x, aes(x = position + 0.5,
-  #                     y = pmax(cumulative, prev),
-  #                     xmin = position + 0.15, xmax = position + 0.85,
-  #                     ymin = cumulative, ymax = prev,
-  #                     fill = sign,
-  #                     label = pretty_text))
-  # # add rectangles and hline
-  # pl <- pl +
-  #   geom_errorbarh(data = x[x$variable_name != "", ],
-  #                  aes(xmax = position - 0.85,
-  #                      xmin = position + 0.85,
-  #                      y = cumulative), height = 0,
-  #                  color = "#371ea3") +
-  #   geom_rect(alpha = 0.9) +
-  #   geom_hline(data = broken_baseline, aes(yintercept = contribution), lty = 3, alpha = 0.5, color = "#371ea3") +
-  #   facet_wrap(~label, scales = "free_y", ncol = 1)
-  #
-  # # add addnotations
-  # if (add_contributions) {
-  #   drange <- diff(range(x$cumulative))
-  #   pl <- pl + geom_text(aes(y = right_side),
-  #                        vjust = 0.5,
-  #                        nudge_y = drange*shift_contributions,
-  #                        hjust = 0,
-  #                        color = "#371ea3")
-  # }
-  #
-  # # set limits for contributions
-  # if (any(is.na(min_max))) {
-  #   x_limits <- scale_y_continuous(expand = c(0.05,0.15), name = "")
-  # } else {
-  #   x_limits <- scale_y_continuous(expand = c(0.05,0.15), name = "", limits = min_max)
-  # }
-  #
-  # if (is.null(vnames)) vnames <- x$variable
-  #
-  # pl <- pl + x_limits +
-  #   scale_x_continuous(labels = vnames, breaks = x$position + 0.5, name = "") +
-  #   scale_fill_manual(values = vcolors)
-  #
-  # # add theme
-  # pl + coord_flip() + DALEX::theme_drwhy_vertical() +
-  #   theme(legend.position = "none") +
-  #   labs(title = title, subtitle = subtitle)
-}
-
-plot_contribution(shaps1[1, ], model = unified_model, x = data[1, 3:7], max_vars = 4)
 
 #' SHAP value based Feature Dependence plot
 #'
@@ -279,3 +129,183 @@ plot_feature_dependence <- function(shaps, x, variable,
 }
 
 #plot_feature_dependence(shaps1, data[1:3, ], variable = "overall")
+
+
+#' SHAP value based Break-Down plot
+#'b
+#' This function plots contributions of features into the prediction of a single observation.
+#'
+#' @param shap SHAP values dataframe produced with the \code{treeshap} function, containing only one row.
+#' @param x \code{NULL} or dataframe with 1 observation used to calculate \code{shap}.
+#' Used only for aesthetic reasons - to include observation values for a different variables next to the variable names in labels on the y axis.
+#' By default is \code{NULL} and then labels on the y axis are just variable names.
+#' @param model \code{NULL} or dataframe containing unified representation of explained model created with a (model).unify function.
+#' @param max_vars maximum number of variables that shall be presented. Variable with highest importance will be presented.
+#' Other variables will be summed into one contribution. By default \code{5}.
+#' @param min_max a range of OX axis. By default \code{NA}, therefore it will be extracted from the contributions of \code{x}.
+#' But it can be set to some constants, useful if these plots are to be used for comparisons.
+#' @param digits number of decimal places (\code{\link{round}}) to be used.
+#' @param title the plot's title, by default \code{'SHAP Break-Down'}.
+#' @param subtitle the plot's subtitle. By default no subtitle.
+#'
+#' @return a \code{ggplot2} object
+#'
+#' @export
+#'
+#' @seealso
+#' \code{\link{treeshap}} for calculation of SHAP values
+#'
+#' @examples
+#' \dontrun{
+#' library(xgboost)
+#' data <- fifa20$data[colnames(fifa20$data) != 'work_rate']
+#' target <- fifa20$target
+#' param <- list(objective = "reg:squarederror", max_depth = 3)
+#' xgb_model <- xgboost::xgboost(as.matrix(data), params = param, label = target, nrounds = 200)
+#' unified_model <- xgboost.unify(xgb_model)
+#' shaps <- treeshap(unified_model, head(data, 3))
+#' plot_feature_importance(shaps, max_vars = 4)
+#' }
+plot_contribution <- function(shap,
+                              x = NULL,
+                              model = NULL,
+                              max_vars = 5,
+                              min_max = NA,
+                              digits = 3,
+                              title = "SHAP Break-Down",
+                              subtitle = "") {
+  #position <- cumulative <- prev <- pretty_text <- right_side <- contribution <- NULL
+
+  if (max_vars > ncol(shap)) {
+    warning("max_vars exceeds number of variables. All variables will be shown.")
+    max_vars <- ncol(shap)
+  }
+
+  if (nrow(shap) != 1) {
+    warning("Only 1 observation can be plotted. Plotting 1st one.")
+    shap <- shap[1, ]
+  }
+
+  # calculating model's mean prediction
+  if (!is.null(model)) {
+    if (!all(c("Tree", "Node", "Feature", "Split", "Yes", "No", "Missing", "Quality/Score", "Cover") %in% colnames(model))) {
+      stop("Given model dataframe is not a correct unified dataframe representation. Use (model).unify function.")
+    }
+    is_leaf <- is.na(model$Feature)
+    is_root <- model$Node == 0
+    mean_prediction <- sum(model[is_leaf, "Quality/Score"] * model$Cover[is_leaf]) / sum(model$Cover[is_root])
+  } else {
+    mean_prediction <- 0
+  }
+
+
+  df <- data.frame(variable = colnames(shap), contribution = as.numeric(shap))
+
+  # if using observation values, then setting variable names to showing their value
+  if (!is.null(x)) {
+    if (!all(colnames(x) == colnames(shap))) {
+      stop("shap and x should have the same variables.")
+    }
+    if (nrow(x) != 1) {
+      warning("Only 1 observation can be plotted. Assuming first observations in x and in shap are the same.")
+      x <- x[1, ]
+    }
+    df$variable <- paste0(df$variable, " = ", as.character(x))
+  }
+
+  # selecting max_vars most important variables
+  is_important <- order(abs(df$contribution), decreasing = TRUE)[1:max_vars]
+  other_variables_contribution_sum <- sum(df$contribution[-is_important])
+  df <- df[is_important, ]
+  df$position <- 2:(max_vars + 1)
+  if (max_vars < ncol(shap)) {
+    df <- rbind(df, data.frame(variable = "+ all other variables",
+                               contribution = other_variables_contribution_sum,
+                               position = max_vars + 2))
+  }
+
+  # adding "prediction" bar
+  df <- rbind(df, data.frame(variable = "prediction",
+                             contribution = mean_prediction + sum(df$contribution),
+                             position = max_vars + 3))
+
+  df$sign <- ifelse(df$contribution >= 0, "1", "-1")
+
+  # adding "intercept" bar
+  if (!is.null(model)) {
+    df <- rbind(df, data.frame(variable = "intercept",
+                               contribution = mean_prediction,
+                               position = 1,
+                               sign = "X"))
+  }
+
+  # ordering
+  df <- df[order(df$position), ]
+
+  # adding columns needed by plot
+  df$cumulative <- cumsum(df$contribution)
+  df$prev <- df$cumulative - df$contribution
+  df$text <- as.character(round(df$contribution, digits))
+  df$text[df$contribution > 0] <- paste0("+", df$text[df$contribution > 0])
+
+  # intercept bar corrections:
+  df$prev[1] <- df$contribution[1]
+  df$text[1] <- as.character(round(df$contribution[1], digits))
+
+  # prediction bar corrections:
+  df$prev[max_vars + 3] <- df$contribution[1]
+  df$cumulative[max_vars + 3] <- df$cumulative[max_vars + 2]
+  df$sign[max_vars + 3] <- "X"
+  df$text[max_vars + 3] <- as.character(round(df$contribution[max_vars + 3], digits))
+
+  # reversing postions
+  # TODO - remove this line and instead reverse above
+  df$position <- max(df$position) - df$position + 1
+
+  # base plot
+  p <- ggplot(df, aes(x = position + 0.5,
+                      y = pmax(cumulative, prev),
+                      xmin = position + 0.15, xmax = position + 0.85,
+                      ymin = cumulative, ymax = prev,
+                      fill = sign,
+                      label = text))
+
+  # add rectangles and hline
+  p <- p +
+    geom_errorbarh(data = df[-(max_vars + 3), ],
+                   aes(xmax = position - 0.85,
+                       xmin = position + 0.85,
+                       y = cumulative), height = 0,
+                   color = "#371ea3") +
+    geom_rect(alpha = 0.9) +
+    geom_hline(data = df[df$variable == "intercept", ], aes(yintercept = contribution), lty = 3, alpha = 0.5, color = "#371ea3")
+
+  # add adnotations
+  drange <- diff(range(df$cumulative))
+  p <- p + geom_text(aes(y = pmax(cumulative,  cumulative - contribution)),
+                       vjust = 0.5,
+                       nudge_y = drange * 0.05,
+                       hjust = 0,
+                       color = "#371ea3")
+
+  # set limits
+
+
+  # set limits for contributions
+  if (any(is.na(min_max))) {
+    x_limits <- scale_y_continuous(expand = c(0.05,0.15), name = "", labels = scales::comma)
+  } else {
+    x_limits <- scale_y_continuous(expand = c(0.05,0.15), name = "", limits = min_max, labels = scales::comma)
+  }
+
+  p <- p + x_limits +
+    scale_x_continuous(labels = df$variable, breaks = df$position + 0.5, name = "") +
+    scale_fill_manual(values = DALEX::colors_breakdown_drwhy())
+
+  # add theme
+  p + coord_flip() + DALEX::theme_drwhy_vertical() +
+    theme(legend.position = "none") +
+    labs(title = title, subtitle = subtitle)
+}
+
+plot_contribution(shaps1[1, ], model = unified_model, x = data[1, 3:7], max_vars = 3, min_max = c(0, 30000))
